e4646bf0c7bc1cd2431f10fec56efbda07f5636a
dec04 <dmedia.decoy@gmail.com>
2021-01-22 15:41:55 +0700

    Общие изменения:   - вроде бы закончит добавления bottom app bar и top app bar;

--- app/src/main/java/com/dec04/gazoil/MainActivity.java
+++ app/src/main/java/com/dec04/gazoil/MainActivity.java
@@ -1,14 +1,29 @@
 package com.dec04.gazoil;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar materialToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        materialToolbar = findViewById(R.id.top_app_bar__material_toolbar);
+        materialToolbar.setNavigationOnClickListener(v -> {
+            // Open navigation drawer
+        });
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host__fragment);
+        NavController navController = navHostFragment.getNavController();
     }
 }
