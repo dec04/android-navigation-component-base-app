e1a67384f82222378501d253ebfed19d72d456b5
dec04 <dmedia.decoy@gmail.com>
2021-01-22 20:38:55 +0700

    Общие изменения:   - добавил основные фрагменты (пустые);

--- app/src/main/java/com/dec04/gazoil/fragments/EnterLoginCodeFragment.java
+++ app/src/main/java/com/dec04/gazoil/fragments/EnterLoginCodeFragment.java
@@ -11,19 +11,21 @@ import com.dec04.gazoil.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link EnterLoginCodeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class EnterLoginCodeFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public EnterLoginCodeFragment() {
         // Required empty public constructor
     }
 
@@ -33,10 +35,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment EnterLoginCodeFragment.
      */
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    // TODO: Rename and change types and number of parameters
+    public static EnterLoginCodeFragment newInstance(String param1, String param2) {
+        EnterLoginCodeFragment fragment = new EnterLoginCodeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -57,6 +60,6 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_enter_login_code, container, false);
     }
 }
--- app/src/main/java/com/dec04/gazoil/fragments/SupportFragment.java
+++ app/src/main/java/com/dec04/gazoil/fragments/SupportFragment.java
@@ -1,4 +1,4 @@
-package com.dec04.gazoil;
+package com.dec04.gazoil.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,12 +7,14 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.dec04.gazoil.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EnterLoginCode#newInstance} factory method to
+ * Use the {@link SupportFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EnterLoginCode extends Fragment {
+public class SupportFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +25,7 @@ public class EnterLoginCode extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public EnterLoginCode() {
+    public SupportFragment() {
         // Required empty public constructor
     }
 
@@ -33,11 +35,11 @@ public class EnterLoginCode extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EnterLoginCode.
+     * @return A new instance of fragment SupportFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EnterLoginCode newInstance(String param1, String param2) {
-        EnterLoginCode fragment = new EnterLoginCode();
+    public static SupportFragment newInstance(String param1, String param2) {
+        SupportFragment fragment = new SupportFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,6 +60,6 @@ public class EnterLoginCode extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enter_login_code, container, false);
+        return inflater.inflate(R.layout.fragment_support, container, false);
     }
 }
