11ce4fe5eeafd1f73749f372a8ff33daed2d0762
dec04 <dmedia.decoy@gmail.com>
2021-01-22 20:24:29 +0700

    Общие изменения:   - закончил top app bar, navigation drawer и bottom app bar;   - изменил тему, надо подумать над цветами;

--- app/src/main/java/com/dec04/gazoil/MainActivity.java
+++ app/src/main/java/com/dec04/gazoil/MainActivity.java
@@ -3,6 +3,7 @@ package com.dec04.gazoil;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -11,15 +12,18 @@ import com.google.android.material.appbar.MaterialToolbar;
 public class MainActivity extends AppCompatActivity {
 
     MaterialToolbar materialToolbar;
+    DrawerLayout navigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigationDrawer = findViewById(R.id.navigation_drawer__drawer_layout);
         materialToolbar = findViewById(R.id.top_app_bar__material_toolbar);
         materialToolbar.setNavigationOnClickListener(v -> {
             // Open navigation drawer
+            navigationDrawer.open();
         });
 
         NavHostFragment navHostFragment =
